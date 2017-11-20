fe49b89e5e9370fdce1fc9d52cb200767709226b
abdurrakib136 <31026379+abdurrakib136@users.noreply.github.com>
2017-11-20 19:48:38 +0600

    local

---
+++ local/assets/js/main.js
@@ -0,0 +1,25 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+
+        $(".menu-toggle-icon").click(function(e){
+            e.preventDefault();
+            $(".menu-toggle").toggleClass("active");
+        });
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
