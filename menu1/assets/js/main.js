b6c038084d0b611957ddcfa8ef229087cb5fa582
abdurrakib136 <31026379+abdurrakib136@users.noreply.github.com>
2017-11-28 20:09:27 +0600

    menu1

---
+++ menu1/assets/js/main.js
@@ -0,0 +1,22 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+        
+         $('#nav').slicknav({
+            allowParentLinks: true,
+         });
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
