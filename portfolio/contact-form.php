df1feb1365d78322b250d74b79b4c7a7306b68b6
abdurrakib136 <31026379+abdurrakib136@users.noreply.github.com>
2018-02-12 12:54:34 +0600

    cheng

--- portfolio/contact-form.php
+++ portfolio/contact-form.php
@@ -1,21 +1,19 @@
 <?php
 // Naming Constant
-define( "RECIPIENT_NAME", "John Doe" );
-define( "RECIPIENT_EMAIL", "abdurrakib528@mail.com" );
-
+define( "RECIPIENT_NAME", "Rakib" );
+define( "RECIPIENT_EMAIL", "abdurrakib528@gmail.com" );
 // Values
 $success = false;
 $senderName = isset( $_POST['name'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_POST['name'] ) : "";
 $senderEmail = isset( $_POST['email'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['email'] ) : "";
 $subject = isset( $_POST['subj'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_POST['subj'] ) : "";
 $message = isset( $_POST['message'] ) ? preg_replace( "/(From:|To:|BCC:|CC:|Subject:|Content-Type:)/", "", $_POST['message'] ) : "";
-
 // Send the email
-if ( $senderName &amp;amp;amp;amp;amp;amp;amp;amp;amp;amp;&amp;amp;amp;amp;amp;amp;amp;amp;amp;amp; $senderEmail &amp;amp;amp;amp;amp;amp;amp;amp;amp;amp;&amp;amp;amp;amp;amp;amp;amp;amp;amp;amp; $message ) {
- $recipient = RECIPIENT_NAME . " &amp;amp;amp;amp;amp;amp;amp;amp;amp;lt;" . RECIPIENT_EMAIL . "&amp;amp;amp;amp;amp;amp;amp;amp;amp;gt;";
- $headers = "From: " . $senderName . " &amp;amp;amp;amp;amp;amp;amp;amp;amp;lt;" . $senderEmail . "&amp;amp;amp;amp;amp;amp;amp;amp;amp;gt;";
+if ( $senderName &amp;amp;&amp;amp; $senderEmail &amp;amp;&amp;amp; $message ) {
+ $recipient = RECIPIENT_NAME . " &amp;lt;" . RECIPIENT_EMAIL . "&amp;gt;";
+ $headers = "From: " . $senderName . " &amp;lt;" . $senderEmail . "&amp;gt;";
  $success = mail( $recipient, $subject, $message, $headers );
  //Set Location
- header('Location: thankyou.html');
+ header('Location: index.html');
 }
 ?>
